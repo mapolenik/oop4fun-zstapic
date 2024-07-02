1b31be604f1b699be4b80ee8b378df6364288f7d
LAPTOP-LEHNIO12\Ucitel <milan.apolenik@oapb.sk>
2024-07-02 17:42:45 +0200

    Uprava aj na dvoch policajtov aj pohybovanie po mensich usekoch

--- Projekt_Game/Police.java
+++ Projekt_Game/Police.java
@@ -8,15 +8,63 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Police extends Actor
 {
+    private String up;
+    private String down;
+    private String left;
+    private String right;
+    private int delay;
+    private int delayCounter;
+    
+    public Police(String up, String down, String left, String right)
+    {
+        this.up = up;
+        this.down = down;
+        this.left = left;
+        this.right = right;
+        this.delay = 3;
+        this.delayCounter = 0;
+    }
     /**
      * Act - do whatever the Police wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        if (Greenfoot.isKeyDown("m"))
+          if (this.delayCounter==this.delay)
         {
+            this.movement();  
+            this.delayCounter=0;
+        } else 
+    {
+        this.delayCounter=this.delayCounter+1;
+        }
+        
+    }
+    
+    public void movement()
+{
+    
+        if (Greenfoot.isKeyDown(this.up))
+        {
+        this.setRotation(270);    
+        this.move(1);
+    } else if (Greenfoot.isKeyDown(this.down))
+    {
+        this.setRotation(90);    
+        this.move(1);
+    } else if (Greenfoot.isKeyDown(this.left))
+    {
+        this.setRotation(180);    
+        this.move(1);
+    }else if (Greenfoot.isKeyDown(this.right))
+    {
+        this.setRotation(0);    
         this.move(1);
     }
+
+
 }
+
+
+
 }
